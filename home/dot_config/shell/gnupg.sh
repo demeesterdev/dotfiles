b07b8ff0edceffe94077014b6310f6c5f57bebbe
demeesterdev <thijs@demeester.dev>
2024-07-26 14:21:27 +0200

    add macos config

--- home/dot_config/shell/gnupg.sh
+++ home/dot_config/shell/gnupg.sh
@@ -5,3 +5,9 @@ gpgconf --launch gpg-agent
 if ! pgrep -x -u "${USER}" gpg-agent &> /dev/null; then
         gpg-connect-agent updatestartuptty /bye &> /dev/null
 fi
+
+if [ -f "${HOME}/.gpg-agent-info" ]; then
+    . "${HOME}/.gpg-agent-info"
+    export GPG_AGENT_INFO
+    export SSH_AUTH_SOCK
+fi
