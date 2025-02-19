ede988590605c2fd860e088942a188e874770de3
demeesterdev <thijs@demeester.dev>
2025-02-19 11:35:13 +0100

    fix shellcheck

--- home/dot_config/shell/nativescript.sh
+++ home/dot_config/shell/nativescript.sh
@@ -2,9 +2,10 @@
 ANDROID_HOME=$HOME/Library/Android/sdk
 if [ -d "$ANDROID_HOME" ]; then
     export ANDROID_HOME
-    export PATH=$PATH:$ANDROID_HOME/platform-tools
+    export PATH="$PATH:$ANDROID_HOME/platform-tools"
 fi
 
 if [ -e "/usr/libexec/java_home" ]; then
-    export JAVA_HOME=$(/usr/libexec/java_home -v"17");
-fi 
+    JAVA_HOME="$(/usr/libexec/java_home -v "17")"
+    export JAVA_HOME;
+fi
