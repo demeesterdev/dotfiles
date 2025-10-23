25682c25221516bd027abee81eee5c1715bdc9d3
demeesterdev <thijs@demeester.dev>
2025-10-23 11:08:40 +0200

    update auth config

--- home/dot_config/shell/nativescript.sh
+++ home/dot_config/shell/nativescript.sh
@@ -6,6 +6,10 @@ if [ -d "$ANDROID_HOME" ]; then
 fi
 
 if [ -e "/usr/libexec/java_home" ]; then
-    JAVA_HOME="$(/usr/libexec/java_home -v "17")"
+    JAVA_HOME="$(/usr/libexec/java_home -v "17" 2>/dev/null)"
+    if [ -z "${JAVA_HOME}" ]; then
+        unset JAVA_HOME
+    else
     export JAVA_HOME;
+    fi
 fi
